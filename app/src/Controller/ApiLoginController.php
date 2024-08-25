998aa1c9dbc2ace68666152358c501caa8177726
Leonardo Kleimpaul <leonardokleimpaul@gmail.com>
2024-08-25 20:41:49 -0300

    Adicionado permissão no cadastro e validado para somente admins poder criar novos usuarios

--- app/src/Controller/ApiLoginController.php
+++ app/src/Controller/ApiLoginController.php
@@ -8,6 +8,7 @@ use App\Service\ApiLoginService;
 use App\Service\UserService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,7 +17,7 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class ApiLoginController extends AbstractController
 {
     #[Route('/api/login', name: 'api_login', methods: ['POST'])]
-    public function index(#[CurrentUser] ?User $user): JsonResponse
+    public function index(#[CurrentUser] ?User $user, Request $request): JsonResponse
     {
         if (null === $user) {
             return $this->json([
@@ -32,29 +33,33 @@ class ApiLoginController extends AbstractController
 
     #[Route('/api/registrar', name: 'api_registrar', methods: ['POST'])]
     public function register(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em): JsonResponse {
-        $data = json_decode($request->getContent(), true);
+        if($this->isGranted('ROLE_ADMIN')) {
+            $data = json_decode($request->getContent(), true);
 
-        $apiLoginService = new ApiLoginService($em);
-        $userService = new UserService($em);
+            $apiLoginService = new ApiLoginService($em);
+            $userService = new UserService($em);
 
-        $errors = $apiLoginService->validateNewUser($data);
+            $errors = $apiLoginService->validateNewUser($data);
 
-        if (count($errors) > 0) {
-            return $this->json([
-                'error' => $errors['error'],
-            ], JsonResponse::HTTP_BAD_REQUEST);
+            if (count($errors) > 0) {
+                return $this->json([
+                    'error' => $errors['error'],
+                ], JsonResponse::HTTP_BAD_REQUEST);
+            }
+
+            // Criar uma nova instância de User
+            $user = new User();
+            $user->setEmail($data['email']);
+            $user->setRoles([$data['role']]);
+
+            $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
+            $user->setPassword($hashedPassword);
+
+            $userService->save($user);
+
+            return $this->json(['message' => 'Usuário criado com sucesso!'], JsonResponse::HTTP_CREATED);
         }
 
-        // Criar uma nova instância de User
-        $user = new User();
-        $user->setEmail($data['email']);
-        $user->setRoles(['ROLE_USER']); // Definindo o papel do usuário como 'ROLE_USER'
-
-        $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
-        $user->setPassword($hashedPassword);
-
-        $userService->save($user);
-
-        return $this->json(['message' => 'Usuário criado com sucesso!'], JsonResponse::HTTP_CREATED);
+        return $this->json(['message' => 'Sem permissão para realizar a ação.'], JsonResponse::HTTP_UNAUTHORIZED);
     }
 }
