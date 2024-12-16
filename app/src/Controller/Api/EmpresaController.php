8706f88a2ce071cb41c144759057245c876ac280
Leonardo Kleimpaul <leonardokleimpaul@gmail.com>
2024-12-15 21:08:16 -0300

    Criando front

--- app/src/Controller/Api/EmpresaController.php
+++ app/src/Controller/Api/EmpresaController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Api;
 
 use App\Entity\Empresa;
 use App\Service\EmpresaService;
--- app/src/Controller/Api/RegistroPontoController.php
+++ app/src/Controller/Api/RegistroPontoController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Api;
 
 use App\Entity\RegistroPonto;
 use App\Service\RegistroPontoService;
